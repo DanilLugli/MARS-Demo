72d7d0adeb83703219e4cdefbd4b099f3eda50a5
DanilLugli <danillugli.77@gmail.com>
2024-09-27 15:31:49 +0200

    first commit

---
+++ ARL Navigation/ARL NavigationTests/ARL_NavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARL_NavigationTests.swift
+//  ARL NavigationTests
+//
+//  Created by Danil Lugli on 27/09/24.
+//
+
+import Testing
+@testable import ARL_Navigation
+
+struct ARL_NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
