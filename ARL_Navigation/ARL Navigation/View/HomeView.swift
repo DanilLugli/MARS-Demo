ed789ec5025d1a3bbd88bb993ac80deae9f8e009
DanilLugli <danillugli.77@gmail.com>
2024-10-17 12:54:57 +0200

    update

--- ARL_Navigation/ARL Navigation/View/HomeView.swift
+++ ARL_Navigation/ARL Navigation/View/HomeView.swift
@@ -10,41 +10,53 @@ struct HomeView: View {
     @State private var arView = ARSCNView()
     
     var body: some View {
-        VStack {
-            if let provider = locationProvider {
-                provider.showMap()
-                Text((locationProvider?.building.name)!)
-            } else {
-                Button(action: {
-                    Task {
-                        let fileManager = FileManager.default
-                        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("ARLNavigation")
+        NavigationView { // Aggiungi NavigationView per la toolbar
+            VStack {
+                if let provider = locationProvider {
+                    provider.showMap()
+                        .ignoresSafeArea()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else {
+                    VStack {
+                        // Testo descrittivo sopra il bottone
+                        Text("ARL Navigation is an app that demonstrates the functionality of the MARS library. Press the button below to Start end to be mapped and localized in the EveryWare Lab map.")
+                            .font(.body)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 20)
+                            .padding(.bottom, 20)
                         
-                        if !fileManager.fileExists(atPath: documentsDirectory.path) {
-                            do {
-                                try fileManager.createDirectory(at: documentsDirectory, withIntermediateDirectories: true, attributes: nil)
-                                print("Directory creata correttamente")
-                            } catch {
-                                // Gestisci l'errore
-                                print("Errore durante la creazione della directory: \(error)")
+                        Button(action: {
+                            Task {
+                                let fileManager = FileManager.default
+                                let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("ARLNavigation")
+                                
+                                if !fileManager.fileExists(atPath: documentsDirectory.path) {
+                                    do {
+                                        try fileManager.createDirectory(at: documentsDirectory, withIntermediateDirectories: true, attributes: nil)
+                                        print("Directory created successfully")
+                                    } catch {
+                                        // Handle error
+                                        print("Error creating directory: \(error)")
+                                    }
+                                }
+                                
+                                let casaDirectoryURL = documentsDirectory
+                                let arSCNView = ARSCNView(frame: .zero) // Initialize ARSCNView
+                                locationProvider = await PositionProvider(url: casaDirectoryURL, arSCNView: arView)
                             }
+                        }) {
+                            Text("Upload Map & Start Navigation")
+                                .font(.headline)
+                                .foregroundColor(.blue) // Text color
+                                .frame(width: 280, height: 80) // Button size
+                                .background(Color.blue.opacity(0.4)) // Background with opacity
+                                .cornerRadius(20) // Rounded corners
                         }
-                        
-                        let casaDirectoryURL = documentsDirectory
-                        let arSCNView = ARSCNView(frame: .zero) // Inizializza ARSCNView
-                        locationProvider = await PositionProvider(url: casaDirectoryURL, arSCNView: arView)
                     }
-                }) {
-                    Text("Upload Map & Start Navigation")
-                        .font(.headline)
-                
-                        .foregroundColor(.blue) // Colore del testo
-                        .frame(width: 280, height: 80) // Dimensioni del pulsante
-                        .background(Color.blue.opacity(0.4)) // Sfondo blu con opacità
-                        .cornerRadius(20) // Bordi arrotondati
+                    .padding()
+                    .navigationTitle("ARL Navigation")
                 }
             }
         }
     }
 }
-
